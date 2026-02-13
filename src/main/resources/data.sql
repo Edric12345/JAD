60e6e4e39891b1382db9b8955fd019ff5477c0f1
Edric <167728615+Edric12345@users.noreply.github.com>
2026-02-13 18:21:34 +0800

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,16 +3,6 @@ INSERT INTO admin_user (username, password)
 VALUES ('admin', 'admin123')
 ON CONFLICT (username) DO NOTHING;
 
--- customers
-INSERT INTO customers (name, email, password, phone, address, care_preferences, emergency_contact, medical_notes) 
-VALUES
-('Alice Tan','alice@example.com','alice123','91234561','123 Clementi Ave',NULL,NULL,NULL),
-('John Lim','john@example.com','john123','98765432','456 Jurong West',NULL,NULL,NULL),
-('Mary Lee','mary@example.com','mary123','90011223','789 Bukit Batok',NULL,NULL,NULL),
-('123','123@gmail.com','123','123','123','No baths after 10 pm ','88635767','Allergic to milk and water')
-ON CONFLICT (email) DO NOTHING;
-
--- caregivers
 
 -- service_category
 INSERT INTO service_category (category_name, description) 
@@ -36,5 +26,6 @@ VALUES
 ((SELECT id FROM service_category WHERE category_name='Mobility & Household Support'),'Home Cleaning Support','General cleaning for elderly homes.',55,'images/service8.png',NULL),
 ((SELECT id FROM service_category WHERE category_name='Mobility & Household Support'),'Transport to Appointments','Escort to clinics/hospitals.',90,'images/service9.png',NULL)
 ON CONFLICT DO NOTHING;
+
 
 
